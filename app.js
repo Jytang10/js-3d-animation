2df7f12c0065fed825adcbb98015bc70e0b6079a
James Tang <jytang10@gmail.com>
2020-10-27 20:13:19 +0900

    add anim in out events'

--- app.js
+++ app.js
@@ -1,10 +1,21 @@
-//Movement animation
+//Movement animation selectors
 const card = document.querySelector(".card");
 const container = document.querySelector(".container");
 
 //Moving animation event
 container.addEventListener("mousemove", (e) => {
-  let xAxis = (window.innerWidth / 2 - e.pageX) / 10;
-  let yAxis = (window.innerHeight / 2 - e.pageY) / 10;
+  let xAxis = (window.innerWidth / 2 - e.pageX) / 25;
+  let yAxis = (window.innerHeight / 2 - e.pageY) / 25;
   card.style.transform = `rotateY(${xAxis}deg) rotateX(${yAxis}deg)`;
+});
+
+//Animate in
+container.addEventListener("mouseenter", (e) => {
+  card.style.transition = "none";
+});
+
+//Animate out
+container.addEventListener("mouseleave", (e) => {
+  card.style.transition = "all 0.5s ease";
+  card.style.transform = `rotateY(0deg) rotateX(0deg)`;
 });
