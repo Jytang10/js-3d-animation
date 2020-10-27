42e84afd2102bebe1aaa5eedaaecaadc8c808632
James Tang <jytang10@gmail.com>
2020-10-27 19:57:57 +0900

    added rotate event

---
+++ app.js
@@ -0,0 +1,10 @@
+//Movement animation
+const card = document.querySelector(".card");
+const container = document.querySelector(".container");
+
+//Moving animation event
+container.addEventListener("mousemove", (e) => {
+  let xAxis = (window.innerWidth / 2 - e.pageX) / 10;
+  let yAxis = (window.innerHeight / 2 - e.pageY) / 10;
+  card.style.transform = `rotateY(${xAxis}deg) rotateX(${yAxis}deg)`;
+});
